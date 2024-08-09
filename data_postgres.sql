1f3e0ce0a7766f69b5c153504d1b3d3333a992a8
Bhoj Karki <bbk2049@gmail.com>
2024-08-09 18:17:52 -0500

    added changes

--- data_postgres.sql
+++ data_postgres.sql
@@ -1,14 +1,3 @@
-##### Creating tables in PostGres
-
-In this tutorial we will follow following diagram
-
-![alt text](TableDiagram.png "Title")
-
-Let's create table:
-
-#### POSTGRES
-
-```sql
 -- CREATE TABLE Customer
 CREATE TABLE Customer (
 	customer_id SERIAL PRIMARY KEY,
@@ -23,7 +12,7 @@ CREATE TABLE Product(
 	name VARCHAR(255) NOT NULL,
 	unit_price FLOAT NOT NULL, -- DEFAULT 0.0
 	category_name VARCHAR(255),
-	description TEXT
+	description TEXT	
 );
 
 -- CREATE TABLE Orders
@@ -35,15 +24,10 @@ CREATE TABLE Orders(
 	ordered_date DATE 
 );
 
-```
 
-#### MYSQL
+-- Insert data into appropriate tables
 
-#### Inserting Data:
-
-##### Customer
-
-```sql
+-- INSERT DATA INTO Customer
 INSERT INTO CUSTOMER VALUES(1, 'James', 'Smith', 'james@gmail.com');
 INSERT INTO CUSTOMER VALUES(2, 'Michael', 'Brown', 'michael@gmail.com');
 INSERT INTO CUSTOMER VALUES(3, 'John', 'Miller', 'john@gmail.com');
@@ -59,11 +43,11 @@ INSERT INTO CUSTOMER VALUES(12, 'Anita', 'Karki', 'anita@gmail.com');
 INSERT INTO CUSTOMER VALUES(13, 'Uogu', 'Wale', 'uogu@gmail.com');
 INSERT INTO CUSTOMER VALUES(14, 'Sanjog', 'Thapa', 'sanjog@gmail.com');
 INSERT INTO CUSTOMER VALUES(15, 'Sai', 'Kumar', 'sai@gmail.com');
-```
 
-#### Product
 
-```sql
+
+
+-- INSERT DATA INTO Product
 INSERT INTO Product VALUES (1, 'Mango', 2.3, 'Fruit', 'Mango fresh from market. 2.3 dollars per pound!');
 INSERT INTO Product VALUES (2, 'Orange', 1.3,  'Fruit','Orange fresh from market. 2.3 dollars per pound!');
 INSERT INTO Product VALUES (3, 'Apple', 2,  'Fruit','Apple fresh from market from california. 2 dollars per pound!');
@@ -76,12 +60,7 @@ INSERT INTO Product VALUES (9, 'Milk', 2.3, 'Grocery', 'Milk fresh from market. 
 INSERT INTO Product VALUES (10, 'Bread', 4.2, 'Grocery', 'Bread fresh from market. 4.2 dollars per bag!');
 
 
-```
-
-
-#### Orders
-
-```sql
+-- INSERT DATA INTO Orders
 INSERT INTO Orders VALUES (1, 1, 1, 1, '2024-01-01');
 INSERT INTO Orders VALUES (2, 1, 2, 3, '2024-01-01');
 INSERT INTO Orders VALUES (3, 3, 10, 2, '2024-01-01');
@@ -102,4 +81,3 @@ INSERT INTO Orders VALUES (13, 3, 9, 2, '2024-02-04');
 INSERT INTO Orders VALUES (14, 15, 5, 3, '2024-03-05');
 INSERT INTO Orders VALUES (15, 8, 6, 2, '2024-03-05');
 INSERT INTO Orders VALUES (16, 10, 9, 2, '2024-03-05');
-```
